acfbc3afc05ae942c0e9ff1f885233d2bbe4fc9c
Yash Bhawsar <yashbhawsar97@gmail.com>
2025-05-12 19:49:25 +0530

    prop drilling

--- base/src/App.jsx
+++ base/src/App.jsx
@@ -1,10 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import Basic from "./Basic";
 
 const App = () => {
   return (
     <>
-      <Basic />
+      <h1>Yash</h1>
     </>
   );
 };
--- base/src/Basic.jsx
+++ base/src/Basic.jsx
@@ -1,33 +1,18 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const Basic = () => {
-  const namesArray = ["Yash", "Priya", "Amit", "Sneha", "Rahul", "Anjali"];
-  const [searchTerm, setSearchTerm] = useState("");
-  const [foundName, setFoundName] = useState(null);
+  const [count, setCount] = useState(0);
 
-  const handleSearch = () => {
-    const nameFound = namesArray.find(
-      (name) => name.toLowerCase() === searchTerm.toLowerCase()
-    );
-    setFoundName(nameFound || "Name not found");
-  };
+  useEffect(() => {
+    console.log("Component Rendered");
+  }, [count]);
 
   return (
-    <div style={{ padding: "20px", fontFamily: "Arial" }}>
-      <h2>Search a Name</h2>
-      <input
-        type="text"
-        value={searchTerm}
-        onChange={(e) => setSearchTerm(e.target.value)}
-        placeholder="Enter name"
-      />
-      <button onClick={handleSearch} style={{ marginLeft: "10px" }}>
-        Search
-      </button>
-      <div style={{ marginTop: "20px" }}>
-        {foundName && <p>Result: {foundName}</p>}
-      </div>
-    </div>
+    <>
+      <h2>The Count is : {count}</h2>
+      <button onClick={() => setCount((c) => c + 1)}>Increment</button>
+      <button onClick={() => setCount((c) => c - 1)}>Decrement</button>
+    </>
   );
 };
 
