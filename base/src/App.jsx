ee4c89f99d9a282d3717f76d0c1a18a3fc55e583
Yash Bhawsar <yashbhawsar97@gmail.com>
2025-05-14 16:27:00 +0530

    API Handling

--- base/src/App.jsx
+++ base/src/App.jsx
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Users from "./Users";
+import PostForm from "./components/PostForm";
+import PostList from "./components/PostList";
 
 const App = () => {
-  return <div>App</div>;
+  const [posts, setPosts] = useState([]);
+  const [editingPost, setEditingPost] = useState(null);
+
+  const fetchPosts = async () => {
+    const res = await axios.get(
+      "https://jsonplaceholder.typicode.com/posts?_limit=5"
+    );
+    setPosts(res.data);
+  };
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  const createPost = async (post) => {
+    const
+  }
+
+  return <></>;
 };
 
 export default App;
