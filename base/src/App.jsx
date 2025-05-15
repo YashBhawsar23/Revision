2a0aa12634b12dc9ffc508db19cc9eb40a437a2d
Yash Bhawsar <yashbhawsar97@gmail.com>
2025-05-15 19:15:06 +0530

    Redux

--- base/src/App.jsx
+++ base/src/App.jsx
@@ -1,13 +1,9 @@
 import React from "react";
-import Movie from "./Movie";
-import UserList from "./UserList";
-import CreatePost from "./CreatePost";
 
 const App = () => {
   return (
     <>
-      {/* <UserList /> */}
-      <CreatePost />
+      <h1>Redux</h1>
     </>
   );
 };
